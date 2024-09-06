fe242142fb36cda36b55fdfbe99352ef8182c2b1
tien <tienvn998@gmail.com>
2024-09-06 22:25:45 +0700

    sending HTTP request via useEffect with async/await

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,14 +1,25 @@
+import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
 
+import { serverUrl } from '../env.json';
+
+
 export default function AvailablePlaces({ onSelectPlace }) {
-  fetch('http://localhost:3000/places').then(res=>res.json()).then(data =>{
-    
-  })
+  const [availablePlaces, setAvailablePlaces] = useState([])
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      const respond = await fetch(`${serverUrl}/places`)
+      const resData = await respond.json()
+      setAvailablePlaces(resData.places)
+    }
+    fetchPlaces()
+  }, [])
 
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
--- src/components/Places.jsx
+++ src/components/Places.jsx
@@ -1,5 +1,7 @@
+import { serverUrl } from '../env.json';
+
 export default function Places({ title, places, fallbackText, onSelectPlace }) {
-  console.log(places);
+
   return (
     <section className="places-category">
       <h2>{title}</h2>
@@ -9,7 +11,7 @@ export default function Places({ title, places, fallbackText, onSelectPlace }) {
           {places.map((place) => (
             <li key={place.id} className="place-item">
               <button onClick={() => onSelectPlace(place)}>
-                <img src={`http://localhost:3000/${place.image.src}`} alt={place.image.alt} />
+                <img src={`${serverUrl}/${place.image.src}`} alt={place.image.alt} />
                 <h3>{place.title}</h3>
               </button>
             </li>
